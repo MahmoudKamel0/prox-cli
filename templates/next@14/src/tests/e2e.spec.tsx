da87c0331799b5ee1867c4a4bb8d32c7e28963bb
Mahmoud Kamel <139923538+MahmoudKamel0@users.noreply.github.com>
2025-12-27 10:27:08 +0200

    Refactor e2e test for HomePage component
    Updated test to use HomePage component and changed test function from 'test' to 'it'.

--- templates/next@14/src/tests/e2e.spec.tsx
+++ templates/next@14/src/tests/e2e.spec.tsx
@@ -1,9 +1,7 @@
-import Home from "@app/page";
-import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import HomePage from "@app/page";
+import { render } from "@testing-library/react";
+import { it } from "vitest";
 
-test("Page", () => {
-    render(<Home />);
-    // Replace "" with the test id you expect to be present in the component
-    expect(screen.getByTestId("home-page")).toBeDefined();
+it("Page", () => {
+    render(<HomePage />);
 });
